926fb634d6a6bb5412558287ab231fcd81ee008a
Alejandro Cartes <alejandroml.cartes@gmail.com>
2024-09-23 18:41:46 -0300

    pep8 improvement

--- utils.py
+++ utils.py
@@ -119,8 +119,8 @@ def secante(func, x0, x1, tol=1e-8, max_iter=1_000, verbose=False):
 
     Notas:
     -----
-    El método de secante requiere dos aproximaciones iniciales de la raíz distintas 
-    que no necesariamente tienen que estar cerca de la raíz real.
+    El método de secante requiere dos aproximaciones iniciales de la raíz
+    distintas que no necesariamente tienen que estar cerca de la raíz real.
 
     Ejemplos:
     --------
@@ -163,18 +163,18 @@ def riemann_sum(func, a, b, n, tipo='right'):
     """
     Función que calcula la suma de Riemann de una función en
     un cierto intervalo [a, b] con n subintervalos.
-    
+
     inputs:
     =======
     func: función a obtener el área bajo la curva
     a, b: límites del intervalo
     n: número de subintervalos
     tipo: tipo de suma de Riemann a calcular (right, left, center)
-    
+
     output:
     =======
     suma_areas: suma de Riemann de la función en el intervalo [a, b]
-    
+
     Ejemplo:
     ========
     >>> f = lambda x: x**2
@@ -185,20 +185,22 @@ def riemann_sum(func, a, b, n, tipo='right'):
     if not (tipo in ['left', 'right', 'center']):
         print("los tipos admitidos son: left, right y center")
         return None
-    
-    suma_areas = 0
+
     dx = (b - a) / n
-    
+    suma_lados = 0
+
     for i in range(n):
         if tipo == 'left':
             x_ev = a + i * dx
-        
+
         elif tipo == 'right':
             x_ev = a + (i + 1) * dx
-        
+
         else:  # center
             x_ev = a + (i + 1/2) * dx
-            
-        suma_areas += dx * func(x_ev)
-    
+
+        suma_lados += func(x_ev)
+
+    suma_areas = suma_lados * dx
+
     return suma_areas
